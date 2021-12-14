58479b8c658a5a10c6e89607bb670ed86ffc2d75
hainv-2854 <nguyen.van.hai@sun-asterisk.com>
2021-12-14 16:31:37 +0700

    Chapter_4_5: Navigation and Bootstrap

--- Gemfile
+++ Gemfile
@@ -26,7 +26,8 @@ gem "jbuilder", "~> 2.7"
 gem "rails-i18n"
 # Use Active Storage variant
 # gem "image_processing", "~> 1.2"
-
+# Gem bootstrap
+gem "bootstrap-sass", "3.4.1"
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", ">= 1.4.4", require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
-    get "static_pages/home"
-    get "static_pages/help"
+    get "home", to: "static_pages#home"
+    get "help", to: "static_pages#help"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
