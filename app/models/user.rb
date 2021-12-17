5c129ab00272c76fae7b878c379f6812b8850e8d
hainv-2854 <nguyen.van.hai@sun-asterisk.com>
2021-12-17 18:47:15 +0700

    Chapter 9: Advanded login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save :email_downcase
+  attr_accessor :remember_token
 
   validates :name,
             presence: true,
@@ -25,6 +26,25 @@ class User < ApplicationRecord
              end
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_column :remember_digest, nil
   end
 
   private
